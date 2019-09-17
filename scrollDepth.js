09d9c66078eb44e7149336ecae7667e5bf7550c8
Rob Saunders <rob.saunders@optimizely.com>
2019-09-17 14:56:28 -0700

    first

---
+++ scrollDepth.js
@@ -0,0 +1,23 @@
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    )
+}
+ 
+var docheight = getDocHeight();
+
+function amountscrolled(){
+    var winheight= window.innerHeight || (document.documentElement || document.body).clientHeight
+    var docheight = getDocHeight()
+    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+    var trackLength = docheight - winheight
+    var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+    console.log(pctScrolled + '% scrolled')
+}
+ 
+window.addEventListener("scroll", function(){
+    amountscrolled();
+}, false)
